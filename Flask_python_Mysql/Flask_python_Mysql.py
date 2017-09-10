b2a961aaff894f605f080b0ee6bc5f0db9a14d74
bhanvanu <31460777+bhanvanu@users.noreply.github.com>
2017-09-10 13:17:15 -0700

    Added new Project

---
+++ Flask_python_Mysql/Flask_python_Mysql.py
@@ -0,0 +1,20 @@
+from flask import Flask, redirect, url_for, request, render_template
+
+#import psycopg2
+#import react from react
+
+
+#Note by default (without template_folder) it searches for html in the templates folder
+# but since i created the html files under project not inside templates or statics i need
+# to provide path for index files
+
+# app = Flask(__name__, template_folder='/Users/tech/PycharmProjects/Flask_python_Mysql')
+app = Flask(__name__)
+@app.route('/')
+def index_page():
+   return render_template('index_page.html')
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+
+
---
+++ Flask_python_Mysql/test_javascript_file.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default class TestComponent extends React.Component {
+
+    render() {
+        return (
+            <div className="content">Test Component</div>
+        )
+    }
+}
