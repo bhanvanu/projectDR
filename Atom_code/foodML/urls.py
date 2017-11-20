ffd5bf36ab7ebdc929fa375632dda6e537aa7e70
bhanvanu <31460777+bhanvanu@users.noreply.github.com>
2017-11-19 17:45:56 -0800

    three html file now linked

--- Atom_code/foodML/urls.py
+++ Atom_code/foodML/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
 
-from .views import index, home_page_view, about_page_view
+from .views import index, home_page_view, about_page_view, dashboard_page_view, analytics_page_view, futureworks_page_view
 
 #the values are from views.py file definitions
 urlpatterns = [
@@ -9,4 +9,9 @@ urlpatterns = [
     url(r'^index/', index),
     url(r'^home/',home_page_view),
     url(r'^about/', about_page_view),
+    url(r'^dashboard/',dashboard_page_view, name='dashboard'),
+    url(r'^analytics/',analytics_page_view, name='analytics'),
+    url(r'^futureworks/',futureworks_page_view, name='futureworks'),
+
+
 ]
